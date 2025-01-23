4b23cd33b3d9b72524779d865ea85694656088dc
Usman Abdullah <usmanabdullbn@gmail.com>
2025-01-24 00:33:54 +0500

    updating

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import AboutSection from "./component/AboutSection";
 import Footer from "./component/Footer";
 import VideoSection from "./component/VideoSection";
 import GallerySection from "./component/GallerySection";
-import BenefitsSection from "./BenefitsSection";
 import AgentsSection from "./component/AgentsSection";
 import DiscoverSection from "./component/DiscoverSection";
 import LocationsSection from "./component/LocationsSection";
 import StatsSection from "./component/StatsSection";
+import BenefitsSection from "./component/BenefitsSection";
 
 const App = () => {
   return (
